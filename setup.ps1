cd0ffd8e1a680be1eb2a61ec946e115d5f62633c
gfa@root.ch <gfa@root.ch>
2025-03-02 15:38:06 +0100

    add dependencies to setup script

--- setup.ps1
+++ setup.ps1
@@ -45,11 +45,8 @@ catch {
     Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
-# OMP Install
-choco install -y oh-my-posh zoxide
-
-# Install github cli
-choco install -y gh
+# install dependencies
+choco install -y oh-my-posh zoxide vim gh
 
 # Font Install
 # Get all installed font families
@@ -61,24 +58,7 @@ $fontFamilies = (New-Object System.Drawing.Text.InstalledFontCollection).Familie
 # Check if CaskaydiaCove NF is installed
 if ($fontFamilies -notcontains "MesloLGM Nerd Font") {
     choco install -y nerd-fonts-meslo 
-    # Download and install CaskaydiaCove NF
-    # $webClient = New-Object System.Net.WebClient
-    # $webClient.DownloadFile("https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/CascadiaCode.zip", ".\CascadiaCode.zip")
-    # 
-    # Expand-Archive -Path ".\CascadiaCode.zip" -DestinationPath ".\CascadiaCode" -Force
-    # $destination = (New-Object -ComObject Shell.Application).Namespace(0x14)
-    # Get-ChildItem -Path ".\CascadiaCode" -Recurse -Filter "*.ttf" | ForEach-Object {
-    #     If (-not(Test-Path "C:\Windows\Fonts\$($_.Name)")) {        
-    #         # Install font
-    #         $destination.CopyHere($_.FullName, 0x10)
-    #     }
-    # }
-
-    # Clean up
-  #  Remove-Item -Path ".\CascadiaCode" -Recurse -Force
-  #  Remove-Item -Path ".\CascadiaCode.zip" -Force
 }
-
 
 # Terminal Icons Install
 #
